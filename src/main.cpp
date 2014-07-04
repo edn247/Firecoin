da1e04894c3ef955a4aa1d0fc3f0b803360e9044
edn247 <edn247@gmail.com>
2014-07-03 23:32:39 -0700

    reduction in block reward for continued PoW
    changed basic PoW reward from 5 to 0.05 so that PoW may continue for effective transaction time and chain safety without destroying the coin's value.

--- src/main.cpp
+++ src/main.cpp
@@ -964,7 +964,7 @@ uint256 WantedByOrphan(const CBlock* pblockOrphan)
 // miner's coin base reward
 int64_t GetProofOfWorkReward(int64_t nFees)
 {
-    int64_t nSubsidy = 5 * COIN;
+    int64_t nSubsidy = 0.05 * COIN;
 	if(pindexBest->nHeight < 2)
     {
         nSubsidy = 500 * COIN;
